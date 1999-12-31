e91d4e8a9bf5ee7a72e631eef2fb7890162be6f2
maigre <thomas.bohl@gmail.com>
2000-01-01 00:59:24 +0100

    fix merge problem

--- player/Multimedia/HPlayer-vlc/main.cpp
+++ player/Multimedia/HPlayer-vlc/main.cpp
@@ -3,27 +3,6 @@
 COMPILE && RUN
 g++ hplayer.c -o hplayer -lvlc && ./hplayer
 */
-#include <string>
-using std::string;
-
-#include <stdio.h>
-#include <iostream>
-using std::getline;
-using std::cout;
-using std::cin;
-using std::endl;
-
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <signal.h>
-#include "dualPlayer.h"
-#include <algorithm>
-
-
-bool done = false;
-dualPlayer *player;
-
 
 // Close players and quit
 void closeAndQuit(int signum) {
@@ -105,73 +84,11 @@ int main(int argc, char* argv[])
 	player = new dualPlayer(argc-1, vlc_argv);
 	inputQueue = new Queue();
 
-<<<<<<< HEAD
-	string input;
-	string command;
-	string argument;
-	while (!done) {
-		getline (cin, input);
-		command = input.substr(0, input.find(" "));
-		if (input.find(" ") != string::npos) argument = input.substr(input.find(" ")+1, input.length());
-		else argument = "";
 
-		if (command == "quit") done = true;
-		else if (command == "load") player->load(argument);
-		else if (command == "play") {
-			if (argument.length() == 0) player->play();
-			else player->play(argument);
-		}
-		else if (command == "stop") player->stop();
-		else if (command == "pause") player->pause();
-		else if (command == "resume") player->resume();
-		else if (command == "toggle") player->togglePause();
-		else if (command == "repeat") {
-			if (argument == "1") player->setRepeat(true);
-			else if (argument == "0") player->setRepeat(false);
-			else cout << "#ERROR_REPEAT " << argument << endl;
-		}
-		else if (command == "volume") 
-		{
-			std::string::size_type sz;
-			int vol = atoi(argument.c_str());
-			if (vol >= 0 and vol <= 200) player->setVolume(vol);
-			else cout << "#ERROR_VOLUME " << argument << endl;
-		}
-		else if (command == "volup") player->volumeUp();
-		else if (command == "voldown") player->volumeDown();
-		else {
-			cout << "Unknown command: " << command << " with arg: " << argument << endl;
-		}
-
-	}
-	closeAndQuit(0);
-
-	/*string filepath = "/home/pi/media/vid1.mp4";
-	string filepath2 = "/home/pi/media/vid2.mp4";
-	player->load(filepath2);
-	sleep(5);
-	player->play();
-	// player->play(filepath);
-	sleep(10);
-
-
-	while(!done) {
-		sleep(5);
-		player->load(filepath2);
-		sleep(5);
-		player->play();
-		sleep(5);
-		player->load(filepath);
-		sleep(2);
-		player->play();
-		done = true;
-	}*/
-=======
 	pthread_create(&consumer, NULL, consume, inputQueue);
 	readcin(inputQueue);
 	pthread_join(consumer,NULL);
   	closeAndQuit(0);
->>>>>>> feature/hplayer
 }
 
 
