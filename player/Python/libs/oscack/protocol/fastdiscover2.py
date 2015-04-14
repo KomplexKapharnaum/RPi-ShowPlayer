e4f43ca928a87ed6b71f5aaeacbad449d1ca902b
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-14 21:13:44 +0200

    correct protocol

--- player/Python/libs/oscack/protocol/fastdiscover2.py
+++ player/Python/libs/oscack/protocol/fastdiscover2.py
@@ -149,7 +149,7 @@ def server_sync(flag):
                             flag_timeout_task_sync.get(
                                 TTL=settings.get("rtp", "timeout") * 1.5, JTL=4))   # TODO check if work
     machine.current_state.preemptible.set()
-    time.sleep(0.1)     # Wait for the client to pass in the correct state
+    time.sleep(0.5)     # Wait for the client to pass in the correct state
     # BEGIN TIME CRITICAL #
     message.send(target, msg_ping.get(**kwargs_ping))
     t_start = rtplib.get_time()
--- player/Python/libs/oscack/protocol/scenariosync.py
+++ player/Python/libs/oscack/protocol/scenariosync.py
@@ -142,9 +142,9 @@ def get_scenario(flag):
     to_get = flag.args["to_get"]
     log.log("debug", "Try to get wia scp : {0}".format(to_get))
     to_get.get_from_distant(flag.args["src"].get_hostname())
-    log.log("raw", "Check is distant group : {0} is same as us {1}".format(to_get.group, flag.args["local_newer"].group))
-    log.log("raw", "Flag args keys : {0}".format(flag.args.keys()))
     if flag.args["local_newer"] is not None:
+        log.log("raw", "Check is distant group : {0} is same as us {1}".format(to_get.group, flag.args["local_newer"].group))
+        log.log("raw", "Flag args keys : {0}".format(flag.args.keys()))
         if "reload" not in flag.args.keys() and flag.args["local_newer"].group == to_get.group:
             log.log("raw", "{0} is same group as us".format(to_get))
             if to_get.dateobj > flag.args["local_newer"].dateobj:
