6eac9ee714b206848870a44d6cbca822a213742a
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-14 18:51:25 +0200

    Clean patcher

--- player/Python/scenario/patcher.py
+++ player/Python/scenario/patcher.py
@@ -95,7 +95,8 @@ class ThreadPatcher(threading.Thread):
                 sendto += [x.uid for x in pool.CURRENT_SCENE.cartes if x.uid not in sendto]
             for dest in sendto:
                 if dest in DNCserver.networkmap.keys():
-                    if dest != settings["uName"] or settings.get("scenario", "dest_self") not in sendto:       # Avoid multiple self send
+                    if dest != settings["uName"] or settings.get(
+                            "scenario", "dest_self") not in sendto:     # Avoid multiple self send
                         message.send(DNCserver.networkmap[dest].target,
                                      message.Message("/signal", signal.uid, ('b', cPickle.dumps(signal, 2)), ACK=True))
                 elif dest != settings.get("scenario", "dest_self"):
