c68c3f2a1094cc2fc47c1bf25f6fdf1020323318
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-16 15:22:19 +0200

    Correct umount

--- player/Python/engine/media.py
+++ player/Python/engine/media.py
@@ -243,9 +243,9 @@ def umount_partitions():
     log.log("raw", "Start to umount partitions")
     sucess = True
     for f in os.listdir(settings.get("path", "usb")):
-        log.log("raw", "found on usb dir : {0}".format(f))
         path = os.path.join(settings.get("path", "usb"), f)
-        if os.path.ismount(f):
+        log.log("raw", "found on usb dir : {0}".format(path))
+        if os.path.ismount(path):
             log.log("raw", "Found directory to umount {0}".format(f))
             umount_cmd = ExternalProcess(name="umount")
             umount_cmd.command += " " + path
