0b0b6539e17fe0273822f1e7583c816c4550f679
SuperPierre <pierre.hoezelle@gmail.com>
2015-04-16 19:41:51 +0200

    debug

--- player/Hardware/hardware/main.cpp
+++ player/Hardware/hardware/main.cpp
@@ -138,7 +138,7 @@ void testRoutine(int n){
 int parseInput(){
   string input;
   getline(cin, input);
-  fprintf(stderr, "\n\nGETCOMMAND : %s",input);
+  fprintf(stderr, "\n\nGETCOMMAND : %s",input.c_str());
   stringstream ss(input);
   string parsedInput;
   ss>>parsedInput;
