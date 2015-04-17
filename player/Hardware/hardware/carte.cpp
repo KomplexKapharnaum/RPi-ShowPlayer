9afccc145adcca0bc5b951412a5eb83bf495b0f0
superpierre <pierre.hoezelle>
2015-04-17 15:27:54 +0200

    pb gamme tension

--- player/Hardware/hardware/carte.cpp
+++ player/Hardware/hardware/carte.cpp
@@ -25,7 +25,7 @@
 void Carte::initCarte(int _pwm_ledb_or_10w2, int _gamme_tension,int checkFloat){
   fprintf(stderr, "carte - add extension card dnc\n");
   SPIcarte.initSPI();
-  SPIcarte.addChipSelect(13,500000);
+  SPIcarte.addChipSelect(13,1000000);
   gamme_tension=_gamme_tension;
   pwm_ledb_or_10w2=_pwm_ledb_or_10w2;
   wiringPiSetupGpio();
@@ -44,6 +44,7 @@ void Carte::initCarte(int _pwm_ledb_or_10w2, int _gamme_tension,int checkFloat){
   delay(5);
   digitalWrite (GPIO_RESET, HIGH);
   delay(50);
+  writeValue(VOLTAGEMODE,gamme_tension);
   checkTension();
   fprintf(stderr, "carte - tension initiale : %f mode : %uV\n", (float)value[UBATT]/10, value[VOLTAGEMODE]);
   writeValue(GYROSPEED,2);
