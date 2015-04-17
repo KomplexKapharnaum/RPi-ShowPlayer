4126c9cd983cd876c96633480630ecc5a044dbd4
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-17 18:25:33 +0200

    Pass osc arg clear and not blob

--- player/Python/engine/media.py
+++ player/Python/engine/media.py
@@ -214,8 +214,8 @@ class Media:
         :return: path, pickle(mtime, filesize)
         """
         log.log("raw", "get_osc_rep for : {0}".format(self))
-        # return ('s', str(self.rel_path)), ('f', float(self.mtime)), ('f', float(self.filesize/1000))
-        return ('s', str(self.rel_path)), ('b', cPickle.dumps((self.mtime, self.filesize), 2))
+        return ('s', str(self.rel_path)), ('f', float(self.mtime)), ('i', int(self.filesize/1000))
+        # return ('s', str(self.rel_path)), ('b', cPickle.dumps((self.mtime, self.filesize), 2))
 
     def put_on_fs(self):  # , error_fnct=None
         """
