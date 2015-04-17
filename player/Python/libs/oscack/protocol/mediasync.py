ce2a9fb0a715d9a6727a386d282395222ad470a1
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-17 17:44:12 +0200

    Add a mtime for all media from fs

--- player/Python/libs/oscack/protocol/mediasync.py
+++ player/Python/libs/oscack/protocol/mediasync.py
@@ -397,7 +397,7 @@ def send_media_list(flag):
     for f in all_media:
         for value in f.get_osc_repr():
             args_list.append(value)             # Add all other media available
-    log.log("raw", "arg list of send media : {0}".append(args_list))
+    log.log("raw", "arg list of send media : {0}".format(args_list))
     message.send(message.Address("255.255.255.255"), msg_media_version.get(args_list))
 
 
