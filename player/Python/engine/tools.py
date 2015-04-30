607000182ce9368b3126d692339097264554678a
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-04-30 19:05:05 +0200

    Display all message

--- player/Python/engine/tools.py
+++ player/Python/engine/tools.py
@@ -218,29 +218,30 @@ class ThreadTeleco(threading.Thread):
             log.warning("Add meesage : {0}".format(message))
             self._pages[page].append(message)
 
-    def _display_message(self, message, page):
+    def _display_message(self, messages, page):
         """
         This function display a message in a page
         :param message: message to display
         :param page: page where the message must be displayed
         :return:
         """
-        message = ThreadTeleco.prepare_message(message)
-        log.warning("Blocs to display : {0}".format(message))
-        for n, bloc in enumerate(message):
-            args = dict()
-            log.log("warning", "Bloc to display : {0}".format(bloc))
-            args["ligne1"] = str(bloc[0])
-            log.log("warning", "Line1 to display : {0}".format(bloc[0]))
-            if len(bloc) > 1:
-                args["ligne2"] = str(bloc[1])
-                log.log("warning", "Line2 to display : {0}".format(bloc[1]))
-            else:
-                args["ligne2"] = ""
-            args["page"] = page
-            engine.threads.patcher.patch(flag_popup.get(args=args))
-            if len(message) >= n + 1:
-                time.sleep(settings.get("log", "teleco", "autoscroll"))
+        for message in messages:
+            message = ThreadTeleco.prepare_message(message)
+            log.warning("Blocs to display : {0}".format(message))
+            for n, bloc in enumerate(message):
+                args = dict()
+                log.log("warning", "Bloc to display : {0}".format(bloc))
+                args["ligne1"] = str(bloc[0])
+                log.log("warning", "Line1 to display : {0}".format(bloc[0]))
+                if len(bloc) > 1:
+                    args["ligne2"] = str(bloc[1])
+                    log.log("warning", "Line2 to display : {0}".format(bloc[1]))
+                else:
+                    args["ligne2"] = ""
+                args["page"] = page
+                engine.threads.patcher.patch(flag_popup.get(args=args))
+                if len(message) >= n + 1:
+                    time.sleep(settings.get("log", "teleco", "autoscroll"))
 
     @staticmethod
     def prepare_message(message):
