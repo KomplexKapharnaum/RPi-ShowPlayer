5dd5f0922134ab4c5e6291bf1fb78fafe523585c
Salvadorpsartek <jeremie.forge@gmail.com>
2015-05-04 17:48:22 +0200

    tooltip grp infos

--- player/Python/interface/www/_TIMELINE/script.js
+++ player/Python/interface/www/_TIMELINE/script.js
@@ -609,7 +609,7 @@
       ///////////////////TOOLTIPS///////////////////////
       this.blockbox.mouseenter(function(e){
         $('.tip').html('');
-        $('.tip').append(thisblock.scene+'<br>');
+        $('.tip').append('Scene: '+thisblock.scene+'<br>'+'Groupe: '+thisblock.group+'<br>'+'Scenarios: '+'<br>');
         $.each(thisblock.scenarios,function(index,scenar){
           $('.tip').append(scenar+'<br>');
         });
