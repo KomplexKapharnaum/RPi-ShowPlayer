d47ce197b703a20cd1d9f7c9f03a362a8744b23a
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-06 18:04:26 +0200

    fix negative dB values with ref and relative volume

--- player/Python/engine/alsa.py
+++ player/Python/engine/alsa.py
@@ -12,18 +12,29 @@ from engine.log import init_log
 log = init_log("alsa")
 
 
-def get_dB(value):
-    sign = ""
+def get_rel_dB(value):
+    """
+    This function return correct string to set volume relativly
+    :param value: value in dB (integer)
+    :return:
+    """
+    sign = "+"
     if value < abs(value):          # Negative
         sign = "-"
     return "{0}dB{1}".format(abs(value), sign)
 
 
 def set_absolute_amixer():
+    """
+    This function set the system volume
+    :return:
+    """
     if settings.get("sys", "raspi"):
         try:
             subprocess.check_call(shlex.split("{cmd} {value}dB".format(cmd=settings.get("path", "amixer"),
-                                                                       value=get_dB(settings.get("sys", "volume")))))
+                                                                       value=settings.get("sys", "ref_volume"))))
+            subprocess.check_call(shlex.split("{cmd} {value}".format(cmd=settings.get("path", "amixer"),
+                                                                     value=get_rel_dB(settings.get("sys", "volume")))))
         except subprocess.CalledProcessError as e:
             log.exception("Cannont set alsamixer volume")
             log.show_exception(e)
