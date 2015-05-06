1b0887c59653cd8737663df53690914ee2e8fab6
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-06 17:44:04 +0200

    fix negative dB values

--- player/Python/engine/alsa.py
+++ player/Python/engine/alsa.py
@@ -12,11 +12,18 @@ from engine.log import init_log
 log = init_log("alsa")
 
 
+def get_dB(value):
+    sign = ""
+    if value < abs(value):          # Negative
+        sign = "-"
+    return "{0}dB{1}".format(abs(value), sign)
+
+
 def set_absolute_amixer():
     if settings.get("sys", "raspi"):
         try:
             subprocess.check_call(shlex.split("{cmd} {value}dB".format(cmd=settings.get("path", "amixer"),
-                                                                       value=settings.get("sys", "volume"))))
+                                                                       value=get_dB(settings.get("sys", "volume")))))
         except subprocess.CalledProcessError as e:
             log.exception("Cannont set alsamixer volume")
             log.show_exception(e)
