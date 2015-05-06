d8d1d5c65551def41deea1d0b26f9059c0eb52f0
superpierre <pierre.hoezelle>
2015-05-06 11:09:26 +0200

    ?

--- player/Hardware/hardware/main.cpp
+++ player/Hardware/hardware/main.cpp
@@ -295,7 +295,7 @@ int parseInput(){
       }
       strncpy(mess1, popup[type][0].c_str(), sizeof(mess1));
       strncpy(mess2, popup[type][1].c_str(), sizeof(mess2));
-      if(type!=0 && myteleco.fisrtView()==0)myteleco.sendString(mess1,mess2,type);
+      if(type!=0)myteleco.sendString(mess1,mess2,type);
     }
 
 
