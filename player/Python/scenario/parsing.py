abf96ac75083f5204a02a51d8a7558d849f8c212
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-07 23:41:42 +0200

    Rename boxname to include box function name

--- player/Python/scenario/parsing.py
+++ player/Python/scenario/parsing.py
@@ -91,7 +91,7 @@ def etapename(box):
     return box['category']+'_'+box['name']
 
 def boxname(scenarioname, box):
-    return (scenarioname+'_'+box['boxname']).upper()
+    return (scenarioname+'_'+box['name']+'_'+box['boxname']).upper()
 
 # 1: DEVICES
 def parse_devices(timeline):
@@ -193,8 +193,8 @@ def parse_scenario(parsepool, name):
             con['connectionLabel'] = None
 
         # TRANSITIONS
-        fromBox = (name+'_'+con['SourceId'].split('_')[1]).upper()
-        toBox = (name+'_'+con['TargetId']).upper()
+        fromBox = (name+'_'+con['From']+'_'+con['SourceId'].split('_')[1]).upper()
+        toBox = (name+'_'+con['To']+'_'+con['TargetId']).upper()
         if fromBox in importEtapes.keys():
             if toBox in importEtapes.keys():
                 importEtapes[fromBox].transitions[con['connectionLabel']] = importEtapes[toBox]
