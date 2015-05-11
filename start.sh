36744c1a991960c97d0ae14e6b432a2a55dcf355
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-11 18:54:00 +0200

    Add test to avoid multiple start.sh instance running

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-./bash/kill.sh
+if [ "$(ps -ejH w | grep start.sh | grep -v grep)" -eq "" ]; then
+    echo "DNC starter begin .."
+else
+    echo "An instance is already running .."
+    echo "EXIT"
+    exit 0
+fi
 
 running=1
 DIRECT_INOUT=0
