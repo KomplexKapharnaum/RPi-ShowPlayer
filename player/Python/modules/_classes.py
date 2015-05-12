cb77a84a9e1f177e2cc195e83e7118e19f43175c
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-12 15:21:30 +0200

    Cast to int for volume

--- player/Python/modules/_classes.py
+++ player/Python/modules/_classes.py
@@ -428,7 +428,7 @@ class AbstractVLC(ExternalProcessFlag):
          :return: Absolute volume for VLC between 0 and 1024
          :rtype: int
         """
-        return int(settings.get("vlc", "volume", "master")) * (int(volume)/100)
+        return int(int(settings.get("vlc", "volume", "master")) * (int(volume)/100))
 
     def set_volume(self, volume):
         """
