47c1b4c0d41c7caaee80638abb45a0e0aec6102b
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-13 13:55:59 +0200

    Kill netctl watchdog before launch it with screen

--- start.sh
+++ start.sh
@@ -16,6 +16,7 @@ if [ $screen -eq 0 ]; then      # There is the GNU screen binary
     echo "Start in a GNU screen session named 'dnc'"
     screen -S dnc -d -m /dnc/dnc.sh -o
     screen -X -S netctl quit
+    sleep 3
     screen -S netctl -d -m /dnc/bash/netctl-watchdog.py
 else
     echo "Start in a classic shell so without the -o option"
