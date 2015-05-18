a67811a4ab168e605ac3f3b6e325b53cab55e7fb
superpierre <pierre.hoezelle>
2015-05-18 16:12:04 +0200

    test sms receive

--- player/Hardware/GPRS_com/main.c
+++ player/Hardware/GPRS_com/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <wiringSerial.h>
 
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
 void readRX(int fd,int end){
   //----- CHECK FOR ANY RX BYTES -----
   if (fd != -1)
@@ -56,59 +64,27 @@ void readRX(int fd,int end){
 int main (int argc, char * argv[]){
   
   int uart0_filestream = -1;
-  
-  
-  char dest[5][13];
-  strcpy(dest[0], "+33678517297");
-  strcpy(dest[1], "+33678517297");
-  strcpy(dest[2], "+33678517297");
-  strcpy(dest[3], "+33678517297");
-  strcpy(dest[4], "+33678517297");
-  
+
+  //serial RX coté RPI tx coté modem pin du haut sur la carte, pin 7 modem
+  //serial TX coté RPI rx coté modem pin du bas sur la carte, pin 8 modem
+
   uart0_filestream = serialOpen ("/dev/ttyAMA0", 19200);
   //serialPrintf (uart0_filestream, "AT+CMGR=1\r\n") ;
-  
-  serialPrintf (uart0_filestream, "AT+CMGF=1\r") ;
-  printf("OUT AT+CMGF=1\n");
-  //while(!serialDataAvail(uart0_filestream));
+  delay(5);
+  //check sim comm
+  serialPrintf (uart0_filestream, "ATI\r") ;
   readRX(uart0_filestream,(int)'K');
-  serialPrintf (uart0_filestream, "AT+CMGD=4\r") ;
-  printf("OUT AT+CMGD=4\n");
-  //while(!serialDataAvail(uart0_filestream));
-  readRX(uart0_filestream,(int)'K');
+  //
   delay(50);
   
   
-  
-  int i=0;
-  int j=0;
-  for (j=0; j<10; j++) {
-    for (i=0; i<5; i++) {
-      serialPrintf (uart0_filestream, "AT+CMGS=\"%s\"\r",dest[i]) ;
-      printf("OUT AT+CMGS=\"%s\"\n",dest[i]);
-      //while(!serialDataAvail(uart0_filestream));
-      readRX(uart0_filestream,(int)'>');
-      serialPrintf (uart0_filestream, "Les temps sont durs, il faut se serrer la ceinture. Il fait froid, le froid de l'hiver. Les sms ne sont pas rapides, c'est pas facile. %i\r",j);
-      printf("OUT Salut%i\r\n",j);
-      //while(!serialDataAvail(uart0_filestream));
-      readRX(uart0_filestream,10);
-      serialPutchar (uart0_filestream, (char)26) ;
-      printf("OUT ‰c\n",(char)26);
-      delay(5);
-      serialPrintf (uart0_filestream, "\r") ;
-      printf("OUT \r\n");
-      //while(!serialDataAvail(uart0_filestream));
-      readRX(uart0_filestream,(int)'K');
-      delay(50);
-    
-    }
-    
+  bool live=true;
+  while(live){
+    readRX(uart0_filestream,(int)'K');
   }
   
   serialClose(uart0_filestream);
-  
-  // Don't forget to clean up
-  //close(uart0_filestream);
+
   return 0;
   
 }
