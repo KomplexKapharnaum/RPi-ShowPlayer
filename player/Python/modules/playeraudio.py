15f0ba76c715ef172cfc55bf6b845d22de6833c0
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2015-05-19 23:32:48 +0200

    fix Filters definition in vlc audio/video player

--- player/Python/modules/playeraudio.py
+++ player/Python/modules/playeraudio.py
@@ -29,6 +29,12 @@ log = init_log("audio")
 #         'AUDIO_END': [True]
 #     }
 
+
+FILTERS = {
+        "MEDIA_END": ["transTo /audio/end", True],
+        "AUDIO_END": [True]
+    }
+
 class AudioVLCPlayer(AbstractVLC):
     """
     This class define an audio player with VLC as backend
@@ -40,18 +46,13 @@ class AudioVLCPlayer(AbstractVLC):
         """:type: dict"""
         arguments.update(settings.get("vlc", "options", "audio"))
         log.log("debug", "Vlc arguments : {0}".format(arguments))
-        AbstractVLC.__init__(self, name="audiovlc", command=command.format(**arguments))
+        AbstractVLC.__init__(self, name="audiovlc", command=command.format(**arguments), filters=FILTERS)
 
     def check_media(self, media):
         """
         Add audio to the media path
         """
         return AbstractVLC.check_media(self, os.path.join(settings.get("path", "relative", "audio"), media))
-
-    Filters = {
-        "MEDIA_END": ["transTo /audio/end", True],
-        "AUDIO_END": [True]
-    }
 
 
 exposesignals(AudioVLCPlayer.Filters)
