d41681551e63b8dc2c7191f972fe62877cd8f7c2
superpierre <pierre.hoezelle>
2015-06-05 15:53:10 +0200

    add debug

--- player/Python/engine/threads.py
+++ player/Python/engine/threads.py
@@ -37,6 +37,7 @@ def stop():
     log.debug("STOPPING THREADS")
     for thread in tools._to_stop_thread.values():
         th = thread()
+        """:type: threading.Thread"""
         log.debug("running : {0}".format(th)) #TODO find why sometime hardware (cf kxkmcard module) do not stop
     for thread in tools._to_stop_thread.values():
         th = thread()   # Get the reference
