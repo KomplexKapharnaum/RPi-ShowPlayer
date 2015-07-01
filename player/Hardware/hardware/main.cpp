0c1ba12ae60835dd28bed79d23c00563efd47d40
superpierre <pierre.hoezelle>
2015-07-01 14:35:51 +0200

    typo

--- player/Hardware/hardware/main.cpp
+++ player/Hardware/hardware/main.cpp
@@ -558,7 +558,7 @@ int parseInput(string input){
 }
 
 void produce(Queue<string>& q, string message) {
-    if(!(message=="interrupt_carte" || message=="interrupt_teleco"|| item=="update_titreur"))fprintf(stderr, "main - prog push %s\n",message.c_str());
+    if(!(message=="interrupt_carte" || message=="interrupt_teleco"|| message=="update_titreur"))fprintf(stderr, "main - prog push %s\n",message.c_str());
     q.push(message);
 }
 
