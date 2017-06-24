ed9d68059aaded13d2131a8a1c6fe5dde3f3e77c
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2017-06-24 11:09:01 +0200

    add teleco in webui

---
+++ player/Python/interface/www/_CTRL/_TELECO/script.js
@@ -0,0 +1,59 @@
+
+  $(function() {
+
+
+
+   /////////////////////////////////////////////////////////////////////
+   /////////////////////////////////////////////////////////////////////
+
+   //////////////////////// GET NAME TIMELINE .///////////////////////
+
+      function add_button(signal, where) {
+          var line = $('<div class="line">');
+          var signalName = signal;
+           if(signalName == "CARTE_PUSH_1"){
+               signalName = "PUSH COURT";
+           }else if(signalName == "CARTE_PUSH_11"){
+               signalName = "PUSH LONG";
+           }else if(signalName == "TELECO_PUSH_OK"){
+               signalName = " OK ";
+           }else if(signalName == "TELECO_PUSH_A"){
+               signalName = " A ";
+           }else if(signalName == "TELECO_PUSH_B"){
+               signalName = " B ";
+           }
+          line.append(signalName);
+         line.data('id', signal);
+         line.on("click", function(e) {
+           var idSignal = $(this).data('id');
+           console.log('clicked '+idSignal);
+             $(this).addClass('lineactive');
+           $.ajax({
+               url: "/sendSignal",
+               type: "POST",
+               data: { signal: idSignal }
+           })
+           .done(function(r) {
+             draw();
+           });
+         });
+         return line;
+      }
+   function draw() {
+
+     $("#teleco").empty();
+     $("#carte").empty();
+
+       $("#teleco").append(add_button("TELECO_PUSH_OK"));
+       $("#teleco").append(add_button("TELECO_PUSH_A"));
+       $("#teleco").append(add_button("TELECO_PUSH_B"));
+
+       $("#carte").append(add_button("CARTE_PUSH_1"));
+       $("#carte").append(add_button("CARTE_PUSH_11"));
+
+
+   }
+
+   draw();
+
+});
