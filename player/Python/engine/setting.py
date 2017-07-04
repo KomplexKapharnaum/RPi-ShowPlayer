469d9a427f59a8f6c7cde8cc3a48407268c1c3be
Olivier@Portable <olivier.radisson@insa-lyon.fr>
2017-07-04 15:34:13 +0200

    set default settings webui->shadow_edit to False

--- player/Python/engine/setting.py
+++ player/Python/engine/setting.py
@@ -201,8 +201,7 @@ DEFAULT_SETTING["webui"] = dict()
 DEFAULT_SETTING["webui"]["autorestart"] = dict()
 DEFAULT_SETTING["webui"]["autorestart"]["enable"] = True    # Auto restart interface
 DEFAULT_SETTING["webui"]["autorestart"]["interval"] = 5     # in sec
-DEFAULT_SETTING["webui"]["shadowedit"] = True     # Auto sync scenario if edited
-#  in webui
+DEFAULT_SETTING["webui"]["shadowedit"] = False     # Auto sync scenario if edited in webui
 
 DEFAULT_SETTING["log"] = dict()
 DEFAULT_SETTING["log"]["level"] = "debug"
